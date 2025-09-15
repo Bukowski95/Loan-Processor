d3737e83adbddafcdd3618051fdc9bae0baecc62
bukowski <strawdog937@gmail.com>
2025-09-15 13:12:54 +0530

    intial commit for the loans microservice

---
+++ loans/src/test/java/com/berozgar/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berozgar.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
